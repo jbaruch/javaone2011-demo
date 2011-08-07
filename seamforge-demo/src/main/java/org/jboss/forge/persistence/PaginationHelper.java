52b977b3b8f17219448ff2e3a76c29e7d07688ae
unknown <Dror@.(none)>
2011-08-07 22:39:51 +0300

    adapting forge generated project to demo L&F

--- seamforge-demo/src/main/java/org/jboss/forge/persistence/PaginationHelper.java
+++ seamforge-demo/src/main/java/org/jboss/forge/persistence/PaginationHelper.java
@@ -1,44 +1,64 @@
 package org.jboss.forge.persistence;
+
 import java.util.List;
+
 public abstract class PaginationHelper<T> {
-  private int pageSize;
-  private int page;
-  public PaginationHelper(  int pageSize){
-    this.pageSize=pageSize;
-  }
-  public abstract int getItemsCount();
-  public abstract List<T> createPageDataModel();
-  public int getPageFirstItem(){
-    return page * pageSize;
-  }
-  public int getPageLastItem(){
-    int i=getPageFirstItem() + pageSize - 1;
-    int count=getItemsCount() - 1;
-    if (i > count) {
-      i=count;
+    private int pageSize;
+    private int page;
+
+    public PaginationHelper(int pageSize) {
+        this.pageSize = pageSize;
     }
-    if (i < 0) {
-      i=0;
+
+    public abstract int getItemsCount();
+
+    public abstract List<T> createPageDataModel();
+
+    public int getPageFirstItem() {
+        return page * pageSize;
     }
-    return i;
-  }
-  public boolean isHasNextPage(){
-    return (page + 1) * pageSize + 1 <= getItemsCount();
-  }
-  public void nextPage(){
-    if (isHasNextPage()) {
-      page++;
+
+    public int getPageLastItem() {
+        int i = getPageFirstItem() + pageSize - 1;
+        int count = getItemsCount() - 1;
+        if (i > count) {
+            i = count;
+        }
+        if (i < 0) {
+            i = 0;
+        }
+        return i;
     }
-  }
-  public boolean isHasPreviousPage(){
-    return page > 0;
-  }
-  public void previousPage(){
-    if (isHasPreviousPage()) {
-      page--;
+
+    public boolean isHasNextPage() {
+        return (page + 1) * pageSize + 1 <= getItemsCount();
     }
-  }
-  public int getPageSize(){
-    return pageSize;
-  }
+
+    public void nextPage() {
+        if (isHasNextPage()) {
+            page++;
+        }
+    }
+
+    public boolean isHasPreviousPage() {
+        return page > 0;
+    }
+
+    public void previousPage() {
+        if (isHasPreviousPage()) {
+            page--;
+        }
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public int getPageCount() {
+        return getItemsCount() / getPageSize();
+    }
 }
